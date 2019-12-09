f19626d1b553206770796dea2ee750b777920de2
Carlos Eduardo <carlos.contato00@gmail.com>
2019-12-08 23:50:16 -0300

    corrigido responsividade para telas de desktop

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: false
+  production: true,
+  ambiente: 'desenvolvimento',
+  webservice: 'http://ec2-52-67-177-208.sa-east-1.compute.amazonaws.com:8080/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   ambiente: 'desenvolvimento',
   webservice: 'http://localhost:8080',
   // webservice: 'http://ec2-52-67-177-208.sa-east-1.compute.amazonaws.com:8080/'
