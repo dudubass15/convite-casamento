88e05739d238e29e7459ecb8680d56676d6e4b06
Carlos Eduardo <carlos.contato00@gmail.com>
2019-12-09 23:17:32 -0300

    adicionado environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   ambiente: 'desenvolvimento',
-  webservice: 'http://ec2-52-67-177-208.sa-east-1.compute.amazonaws.com:8080/'
+  webservice: 'http://ec2-52-67-177-208.sa-east-1.compute.amazonaws.com:8080'
 };
